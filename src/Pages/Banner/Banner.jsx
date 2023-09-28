f50fcfb7bef7528c37ba15941ae8f9d03af3a830
Rakib400372 <rakibulislamr83@gmail.com>
2023-09-28 18:02:53 +0600

    header update

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -3,26 +3,27 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div id='banner' className='p-12'>
-           <div className='text-center'>
-                <p className='text-white pb-5'>We Have The Best NFT Digital Artists</p>
-                <p className='text-gray-400 text-sm px-56 pb-5'>Our artists process a remarkable ability to maarge cutting-edge technology with artist expression,resulting in captiviting and visually stunning artworks.</p>
-           </div>
-           <div className=' flex justify-center'>
-                <div className='px-5'>
-                    <p className='font-bold text-3xl text-white'>10K+</p>
-                    <p className='text-orange-500'>Rare NFT's</p>
-                </div>
-                <div className='px-5'>
-                    <p className='font-bold text-3xl text-white'>50K+</p>
-                    <p className='text-orange-500'>Products</p>
-                </div>
-                <div className='px-5'>
-                    <p className='font-bold text-3xl text-white'>08K+</p>
-                    <p className='text-orange-500'>Artistss</p>
-                </div>
-           </div>
-        </div>
+        <div id='banner' className='p-4 sm:p-6'>
+  <div className='text-center'>
+    <p className='text-white text-lg sm:text-xl pb-3 sm:pb-5'>We Have The Best NFT Digital Artists</p>
+    <p className='text-gray-400 text-sm sm:text-base px-3 sm:px-56 pb-3 sm:pb-5'>Our artists possess a remarkable ability to merge cutting-edge technology with artistic expression, resulting in captivating and visually stunning artworks.</p>
+  </div>
+  <div className='flex flex-row sm:flex-row justify-center'>
+    <div className='p-3 sm:p-5'>
+      <p className='font-bold text-xl sm:text-3xl text-white'>10K+</p>
+      <p className='text-orange-500'>Rare NFT's</p>
+    </div>
+    <div className='p-3 sm:p-5'>
+      <p className='font-bold text-xl sm:text-3xl text-white'>50K+</p>
+      <p className='text-orange-500'>Products</p>
+    </div>
+    <div className='p-3 sm:p-5'>
+      <p className='font-bold text-xl sm:text-3xl text-white'>08K+</p>
+      <p className='text-orange-500'>Artists</p>
+    </div>
+  </div>
+</div>
+
     );
 };
 
--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,53 +1,51 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-
-
-
 // import required modules
 import { EffectCards } from 'swiper/modules';
-
 import './Header.css'
-
-
-
 
 const Header = () => {
     return (
-        <div className='flex'>
-            <div className='pl-40 pt-20'>
-                <p className='text-start tracking-wider'><span className='text-4xl font-bold'>Mint  </span>   <span className='text-4xl font-light'>And  </span> <span className='text-4xl font-bold'>Sell </span><br />
-                <div className='flex pt-2'>
-                   <span className='text-4xl font-bold'>Web3  </span>
-                   <img src="https://i.ibb.co/n6ZQbsH/header-img1.png" alt="" className='h-10 w-40 gap-2   rounded-full'/>
-                </div>
-                <div className='flex pt-2'>
-                    <img src="https://i.ibb.co/Y0zd13S/header-img2.png" alt="" className='h-9 w-25 '/>
-                    <span className='text-4xl font-light ps-3 font-kristi'>Digital Art</span>
-                </div>
-                </p>
-                <p className='text-sm pt-5 text-gray-500'>Minting and selling web3 digital art involves creating unique artworks using blockchain technology and arketing them available for purchase on decentralize platform</p>
-                <div className='pt-5'>
-                   <button type="button" className="relative rounded bg-orange-500 px-4 py-2 text-white hover:text-white hover:bg-orange-600 border-none ">Explore The Market</button>
-                </div>
+        <div className='flex justify-center gap-x-16 m-4'>
+        <div className='pt-20'>
+            <p className='text-start tracking-wider'><span className='text-4xl font-bold'>Mint  </span>   <span className='text-4xl font-light'>And  </span> <span className='text-4xl font-bold'>Sell </span><br />
+            <div className='flex pt-2'>
+               <span className='text-4xl font-bold'>Web3  </span>
+               <img src="https://i.ibb.co/n6ZQbsH/header-img1.png" alt="" className='h-10 w-40 gap-2   rounded-full'/>
             </div>
-         
-            <div className='px-40 py-5'>
-                <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]}className="mySwiper">
-                    <SwiperSlide><img src="https://i.pinimg.com/474x/b6/45/d0/b645d01a3f2175105e52f387dadb594e.jpg" alt="" /></SwiperSlide>
-                    <SwiperSlide><img src="https://i.pinimg.com/736x/35/8f/ce/358fce98df0fb868c70e5c40322cae9a.jpg" alt="" /></SwiperSlide>
-                    <SwiperSlide><img src="https://i.pinimg.com/564x/02/9f/d8/029fd8d72f4d327afe6df991ea555d8f.jpg" alt="" /></SwiperSlide>
-                    <SwiperSlide><img src="https://i.pinimg.com/564x/8a/1f/ea/8a1fea7ce50ead78719c6bb92debf294.jpg" alt="" /></SwiperSlide>
-                   
-                </Swiper>
+            <div className='flex pt-2'>
+                <img src="https://i.ibb.co/Y0zd13S/header-img2.png" alt="" className='h-9 w-25 '/>
+                <span className='text-4xl font-light ps-3 font-kristi'>Digital Art</span>
             </div>
-     
-            
+            </p>
+            <p className='text-sm pt-5 text-gray-500 w-96'>Minting and selling web3 digital art involves creating unique artworks using blockchain technology and arketing them available for purchase on decentralize platform</p>
+            <div className='pt-5'>
+               <button type="button" className="relative rounded bg-orange-500 px-4 py-2 text-white hover:text-white hover:bg-orange-600 border-none ">Explore The Market</button>
+            </div>
         </div>
+     
+        <div >
+            <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]}className="mySwiper">
+                <SwiperSlide><img src="https://i.pinimg.com/474x/b6/45/d0/b645d01a3f2175105e52f387dadb594e.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="https://i.pinimg.com/736x/35/8f/ce/358fce98df0fb868c70e5c40322cae9a.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="https://i.pinimg.com/564x/02/9f/d8/029fd8d72f4d327afe6df991ea555d8f.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="https://i.pinimg.com/564x/8a/1f/ea/8a1fea7ce50ead78719c6bb92debf294.jpg" alt="" /></SwiperSlide>
+            </Swiper>
+        </div>
+ 
+        
+    </div>
+
+
+
+
+
+
+    
     );
 };
 
