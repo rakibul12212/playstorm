c8f0ea1c4da132597dd4c4b6a38cfeee38eb02af
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-05-24 03:15:29 +0600

    j

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import Home from '../Pages/Home/Home';
+
 import Navbar from './Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer/Footer';
